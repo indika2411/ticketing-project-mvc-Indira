79000acc939a8af3e414cb8bcd60dcde83a252af
Indira Carman <indika2411@yahoo.com>
2022-09-30 00:09:15 -0400

    template repo created

---
+++ src/main/java/com/cydeo/TicketingProjectMVCIndira.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectMVCIndira {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMVCIndira.class, args);
+    }
+
+}
